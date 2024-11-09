6ce9c24476fcaf1c8036f46bd1d9988abc40901e
Ala Belgacem <alaaeddine.belgacem@esprit.tn>
2024-11-09 10:58:46 +0100

    Updated tokens

--- llama2.py
+++ llama2.py
@@ -1,6 +1,6 @@
 from huggingface_hub import InferenceClient
 
-client = InferenceClient(model="meta-llama/Llama-2-7b-chat-hf", token="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")
+client = InferenceClient(model="meta-llama/Llama-2-7b-chat-hf", token="hf_xxxxxxxxxxxxxxxxxxxxxxxxxxxx")
 
 # Send a request to the model
 response = client.text_generation("Hello! How can I assist you today?")
