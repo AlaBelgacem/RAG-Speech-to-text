581bd99590b65e40e45c588017b8b8aa5d4e748b
Ala Belgacem <alaaeddine.belgacem@esprit.tn>
2024-11-12 15:21:05 +0100

    Speech to text integration

---
+++ engine/command.py
@@ -0,0 +1,29 @@
+from engine.features import *
+import speech_recognition as sr
+
+
+@eel.expose
+def takecommand():
+
+    r = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print('listening....')
+        eel.DisplayMessage('listening....')
+        r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source)
+        
+        audio = r.listen(source, 10, 6)
+
+    try:
+        print('recognizing')
+        eel.DisplayMessage('recognizing....')
+        query = r.recognize_google(audio, language='en-in')
+        print(f"user said: {query}")
+        eel.DisplayMessage(query)
+        time.sleep(2)
+       
+    except Exception as e:
+        return ""
+    
+    return query.lower()
